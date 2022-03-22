94f5795db9e179c53adde0cde0b42613c6df3efd
ann3i1999 <anneisha.akson@gmail.com>
2022-03-23 01:51:48 +0800

    question 4

---
+++ src/Week1/Vehical.java
@@ -0,0 +1,33 @@
+package Week1;
+
+//QUESTION 4
+public abstract class Vehical {
+
+    private double maxSpeed;
+    protected double currentSpeed;
+
+    Vehical(double maxSpeed){
+        this.maxSpeed = maxSpeed;
+    }
+
+    public abstract void accelerate();
+
+    public double getCurrentSpeed(){
+        return currentSpeed;
+    }
+
+    public double getMaxSpeed(){
+        return maxSpeed;
+    }
+
+    public void pedalToTheMetal(){
+        if (currentSpeed == maxSpeed)
+            return;
+        else
+            accelerate();
+
+    }
+
+
+
+}
