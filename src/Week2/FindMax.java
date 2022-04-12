16233f20398e1a44e04afbcbceb793535a3ad631
ann3i1999 <anneisha.akson@gmail.com>
2022-04-12 23:52:48 +0800

    week 5

--- src/Week2/FindMax.java
+++ src/Week2/FindMax.java
@@ -3,4 +3,19 @@ package Week2;
 //LAB QUESTION 5
 
 public class FindMax {
+
+
+
+    public static class Cirlce implements Comparable <Cirlce> {
+        double radius;
+
+        public Cirlce (double radius){
+            this.radius=radius;
+        }
+
+        @Override
+        public int compareTo(Cirlce o) {
+            return 0;
+        }
+    }
 }
